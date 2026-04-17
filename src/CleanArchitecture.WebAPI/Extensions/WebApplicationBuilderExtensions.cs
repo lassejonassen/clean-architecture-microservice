00aaea9172d42680ca867f14652393177c080c3c
Lasse Jonassen <lassejonassentech@gmail.com>
2026-04-17 02:22:02 +0200

    OTEL implemented but not working

--- src/CleanArchitecture.WebAPI/Extensions/WebApplicationBuilderExtensions.cs
+++ src/CleanArchitecture.WebAPI/Extensions/WebApplicationBuilderExtensions.cs
@@ -2,9 +2,14 @@
 using CleanArchitecture.Infrastructure;
 using CleanArchitecture.SharedKernel;
 using CleanArchitecture.SharedKernel.Messaging;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 using System.Text.Json.Serialization;
 
 namespace CleanArchitecture.WebAPI.Extensions;
+
 public static class WebApplicationBuilderExtensions
 {
     public static WebApplicationBuilder AddDefaults(this WebApplicationBuilder builder, string serviceName)
@@ -24,6 +29,41 @@ public static class WebApplicationBuilderExtensions
 
         builder.Services.AddMediator();
         builder.Services.AddProblemDetails();
+
+
+
+        builder.Services.AddOpenTelemetry()
+            .ConfigureResource(r => r.AddService(serviceName))
+            .WithTracing(tracing => tracing
+                .AddAspNetCoreInstrumentation()
+                .AddHttpClientInstrumentation()
+                .AddOtlpExporter(opt =>
+                {
+                    opt.Endpoint = new Uri("http://aspire-dashboard:4317");
+                }))
+            .WithMetrics(metrics => metrics
+                .AddAspNetCoreInstrumentation()
+                .AddHttpClientInstrumentation()
+                .AddRuntimeInstrumentation() // <--- Needs the NuGet package above
+                .AddOtlpExporter(opt =>
+                {
+                    opt.Endpoint = new Uri("http://aspire-dashboard:4317");
+                }));
+
+        var resourceBuilder = ResourceBuilder.CreateDefault()
+                .AddService(serviceName);
+
+        builder.Logging.AddOpenTelemetry(logging =>
+        {
+            logging.SetResourceBuilder(resourceBuilder);
+            logging.AddOtlpExporter(opt =>
+            {
+                opt.Endpoint = new Uri("http://aspire-dashboard:4317");
+            });
+            // Optional: include attributes like "userId" in logs
+            logging.IncludeFormattedMessage = true;
+            logging.IncludeScopes = true;
+        });
 
         builder.Services.AddControllers()
             .AddJsonOptions(options =>
